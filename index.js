47e864056b3375eb9fec802f081506b0c709a7f1
Luis Vega <luis@port9.com>
2017-09-12 14:26:07 -0700

    meh

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fetch = require('node-fetch')
 const app = express()
 const router = express.Router()
 const logger = require('morgan')
+const consul = require('consul')({ host: process.env.CONSUL || '172.31.7.114'})
+
+let known_zeta_instances = []
 
 app.use(logger('combined'));
 app.use(bodyParser.json());
@@ -18,6 +21,10 @@ router.get('/', (req, res, next) => {
   }})
 })
 
+router.get('/zeta', (req, res, next) => {
+  res.status(200).json(known_zeta_instances)
+})
+
 router.get('/data', (req, res, next) => {
   fetch('http://localhost:4000/secrets')
   .then(response => response.json())
@@ -29,3 +36,20 @@ app.use('/', router)
 app.listen(3001, () => {
   console.log('Listening on port: 3001')
 })
+
+const watcher = consul.watch({
+  method: consul.health.service,
+  options: {
+    service: 'zeta',
+    passing: true
+  }
+})
+
+watcher.on('change', data => {
+  console.log('Change on zeta service:', data.length)
+  known_zeta_instances = [];
+  known_zeta_instances = data.map(s => s.Service.Address + ':' + s.Service.Port)
+})
+
+watcher.on('error', err => console.error('watch error', err))
+
